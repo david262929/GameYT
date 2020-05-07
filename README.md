385e4de83be84a385bfdf06b118df46027465aa6
David <davidghazaryan2000@gmail.com>
2020-05-07 05:27:12 +0400

    installed package json

---
+++ README.md
@@ -0,0 +1,35 @@
+# YerevanTonavachar FB Instant Game
+
+
+## Development
+#### Examples
+- DEVELOPMENT
+```bash
+$ yarn dev                       
+```
+```bash
+$ yarn dev --watch     //  Watch for changes and recompile
+```
+```bash
+$ yarn dev-server      //  Open host "localhost:4200" with watch mode      
+```
+- Production
+```bash 
+$ yarn build                     //  "Production" Mode
+```
+
+
+## Install
+
+Need to be installed in your OS: [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git), [Yarn](https://classic.yarnpkg.com/en/docs/install/#mac-stable) 
+
+For install project modules for development: 
+```bash
+$ yarn install
+```
+
+#### Additional 
+Install [ZSH](https://ohmyz.sh/) for easy work with Git
+
+
+ 
---
+++ weback.config.js
@@ -0,0 +1,71 @@
+const EXPORT_PATH = './dist';
+const EXPORT_FILE_NAME = 'app.min.js';
+const TEMPLATE_HTML = "./public/index.html";
+
+const MODE = process.env.NODE_ENV || 'development';
+const PATH = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+
+let config = {
+    mode: MODE,
+    entry: PATH.resolve(__dirname, './src/index.js'),
+    output: {
+        filename: EXPORT_FILE_NAME,
+        path: PATH.resolve(__dirname, EXPORT_PATH),
+    },
+    resolve: {
+        extensions: ['.js', '.scss', '.css'],
+        alias: {
+            '@modules': PATH.resolve(__dirname, 'src/modules'),
+            '@': PATH.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
+    devServer: {
+        port: 4200,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: TEMPLATE_HTML,
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                exclude: /node_module/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+            {
+                test: /\.(sc|sa|c)ss$/i,
+                exclude: /node_module/,
+                use: [
+                    'style-loader', // Inject Styles into DOM
+                    'css-loader', // Turn css into js
+                    {
+                        loader: 'sass-loader', // Turn scss to css
+                        options: {
+                            implementation: require('sass'),
+                        },
+                    },
+                ],
+            },
+        ]
+    }
+};
+
+if (MODE === 'development') {
+    config.devtool = 'source-map';
+}
+
+module.exports = config;
